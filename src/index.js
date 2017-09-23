2dad549a46f6cacf9a3924c12612aa9db4293c8d
William Elliott <wcelliott1991@gmail.com>
2017-09-23 15:01:16 -0400

    connects to network to return requests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+<Provider store={createStoreWithMiddleware(rootReducer)}>
+    <App />
+</Provider>, 
+document.getElementById('root'));
+registerServiceWorker('.App');
+
---
+++ src/reducers/reducerWeather.js
@@ -0,0 +1,6 @@
+export default function(state = null, action) {
+    console.log('Action recived', action);
+
+    
+    return state;
+}
